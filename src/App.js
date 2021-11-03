aa1d1299f508f2b1a73b42b1d1bab366176a5270
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2021-11-03 20:41:18 +0700

    test hosting

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>HELLO WORLD</h1>
+        <h2>Test firebase hosting</h2>
       </header>
     </div>
   );
