85d9b12aa67f6dfe426c69e4dadc93c9e4327ab9
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2021-12-08 18:04:35 +0700

    name search and pagination

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -10,17 +10,25 @@ import {
 	Card,
 	Placeholder,
 } from "react-bootstrap";
-import ProductCard from "../components/ProductCard/ProductCard";
+
+
 import api from "../api";
-import "./Product.style.css";
 import placeholderImg from "../components/ProductCard/placeholder-image.png";
+import ProductCard from "../components/ProductCard/ProductCard";
+import "./Product.style.css";
 
 const tags = ["tag 1", "tag 2", "tag 3"];
 
 const Products = () => {
-	const [products, setProducts] = useState([]);
-	const [pageItems, setPageItems] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [range, setRange] = useState({
+		min: 0,
+		max: 1,
+	});
+	const [products, setProducts] = useState([]);
+	const [searchTerm, setTerm] = useState("");
+	const [searchResults, setResults] = useState([]);
+	const [pageItems, setPageItems] = useState([]);
 	const [totalPage, setTotalPage] = useState(0);
 	const [currentPage, setCurrentPage] = useState(1);
 	const pageSize = 4;
@@ -34,6 +42,9 @@ const Products = () => {
 				setPageItems(getPageItems(res.data.products, 1));
 				setTotalPage((res.data.products.length + pageSize - 1) / pageSize);
 				setIsLoading(false);
+
+				const [min, max] = getMinMaxPrice(res.data.products);
+				setRange({ min, max });
 			}
 		} catch (err) {
 			console.log(err);
@@ -45,30 +56,59 @@ const Products = () => {
 		// eslint-disable-next-line
 	}, []);
 
+	const getMinMaxPrice = (products) => {
+		const max = Math.max.apply(
+			Math,
+			products.map((product) => product.discountPrice)
+		);
+
+		const min = Math.min.apply(
+			Math,
+			products.map((product) => product.discountPrice)
+		);
+
+		return [min, max];
+	};
+
 	const getPageItems = (array, pageNumber) => {
 		const offset = (pageNumber - 1) * pageSize;
 		return array.slice(offset, offset + pageSize);
 	};
 
 	const handlePageChange = (number) => {
+		const items = searchTerm ? searchResults : products;
+		setPageItems(getPageItems(items, number));
 		setCurrentPage(number);
-		setPageItems(getPageItems(products, number));
 	};
 
-	let items = [];
-	for (let number = 1; number <= totalPage; number++) {
-		items.push(
-			<Pagination.Item
-				key={number}
-				active={number === currentPage}
-				onClick={() => {
-					handlePageChange(number);
-				}}
-			>
-				{number}
-			</Pagination.Item>
-		);
-	}
+	const handleSearchInput = (e) => {
+		setTerm(e.target.value);
+		const searchRegExp = new RegExp(e.target.value, "ig");
+		const results = products.filter((item) => item.name.match(searchRegExp));
+		setResults(results);
+		setTotalPage((results.length + pageSize - 1) / pageSize);
+		setPageItems(getPageItems(results, 1));
+		setCurrentPage(1);
+	};
+
+	const PaginationComp = ({ total, current }) => {
+		let items = [];
+		for (let number = 1; number <= total; number++) {
+			items.push(
+				<Pagination.Item
+					key={number}
+					active={number === current}
+					onClick={() => {
+						handlePageChange(number);
+					}}
+				>
+					{number}
+				</Pagination.Item>
+			);
+		}
+
+		return <Pagination>{items}</Pagination>;
+	};
 
 	const PlaceholderCard = () => (
 		<Col xs={12} lg={6} className="p-4">
@@ -98,14 +138,20 @@ const Products = () => {
 		</Col>
 	);
 
+	const ProductList = ({ items }) => {
+		return items.map((item) => <ProductCard key={item._id} data={item} />);
+	};
+
 	return (
 		<>
 			<Helmet>
 				<title>Voucher Shop - Products</title>
 			</Helmet>
 			<Container className="product-content d-flex mt-4">
+				{/* Side nav */}
 				<div className="product-side-nav">
-					<div className="tag-section">
+					{/* Tags */}
+					<div className="tag-section mb-4">
 						<div className="tag-section-header mb-2 text-center">Tags</div>
 						<Form>
 							<Row>
@@ -128,13 +174,25 @@ const Products = () => {
 							</Row>
 						</Form>
 					</div>
+					{/* Range slider */}
+					<div className="slider-section">
+						<div className="tag-section-header mb-2 text-center">
+							Price range
+						</div>
+						<div className="mt-3 px-2">
+
+						</div>
+					</div>
 				</div>
+
+				{/* Products section */}
 				<div className="product-main">
 					<Form className="w-100 px-3">
 						<FormControl
 							type="search"
 							placeholder="Search"
 							aria-label="Search"
+							onChange={handleSearchInput}
 						/>
 					</Form>
 					<Row className="product-list">
@@ -144,13 +202,13 @@ const Products = () => {
 								<PlaceholderCard />
 							</>
 						) : (
-							pageItems.map((item) => (
-								<ProductCard key={item._id} data={item} />
-							))
+							<ProductList items={pageItems} />
 						)}
 					</Row>
+
+					{/* Pagination */}
 					<div className="d-flex justify-content-center">
-						<Pagination>{items}</Pagination>
+						<PaginationComp total={totalPage} current={currentPage} />
 					</div>
 				</div>
 			</Container>
